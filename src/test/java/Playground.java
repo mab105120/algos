b54987569740cb7c9e34a94c63663d3ef45682bb
mab105120 <mohd.bourji@gmail.com>
2017-03-05 19:32:30 -0800

    clean out sorting tests ... aggregate all tests into one class

--- src/test/java/Playground.java
+++ src/test/java/Playground.java
@@ -1,54 +1,7 @@
-import edu.princeton.cs.introcs.In;
-import org.apache.commons.io.FileUtils;
-import org.junit.Test;
-import sorting.Sort;
-import sorting.SortTest;
-
-import java.io.File;
-import java.io.IOException;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-
 /**
  * Created by Moe on 2/22/17.
  */
 public class Playground {
 
-    @Test
-    public void test() throws IOException {
-        String[] s = new String[]{"a", "d", "f", "c", "b", "e"};
-        sort(s);
-        assertTrue(SortTest.isSorted(s));
-
-        String[] letters = FileUtils.readFileToString(new File("/Users/Moe/Desktop/letters.txt"))
-                .split(" ");
-        assertEquals(1000, letters.length);
-        sort(letters);
-        assertTrue(SortTest.isSorted(letters));
-
-    }
-
-    @Test
-    public void t() throws IOException {
-        final String FILE_PATH = "src/test/resource/letters.txt";
-        In in = new In(new File(FILE_PATH));
-        String[] s = in.readAllStrings();
-    }
-
-    private <T extends Comparable<T>> void sort(T[] t) {
-        int h = 1;
-        while (h < t.length) h = h * 3 + 1;
-        while (h >= 1) {
-            for (int i = h; i < t.length; i++) {
-                int j = i;
-                while (j >= h && Sort.less(t[j], t[j - h])) {
-                    Sort.exchange(t, j, j - h);
-                    j -= h;
-                }
-            }
-            h = h / 3;
-        }
-    }
 
 }
--- src/test/java/sorting/SortTest.java
+++ src/test/java/sorting/SortTest.java
@@ -4,39 +4,55 @@ import edu.princeton.cs.introcs.In;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.function.Consumer;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class SortTest {
 
-	@Test
-	public void lessTest() { 
-		assertTrue(Sort.less(1,2));
-	}
-	
-	@Test
-	public void equalItems() { 
-		assertFalse(Sort.less(1,1));
-	}
-	
-	@Test
-	public void exchange() { 
-		Integer[] i = new Integer[]{1,2,3,4,5,6};
-		Sort.exchange(i, 4, 5);
-		assertTrue(i[4] == 6);
-		assertTrue(i[5] == 5);
-	}
-	
-	public static <T extends Comparable<T>> boolean isSorted(T[] a) { 
-		for (int i = 1; i < a.length; i++) 
-			if (Sort.less(a[i], a[i-1])) return false;
-		return true;
-	}
+    @Test
+    public void lessTest() {
+        assertTrue(Sort.less(1, 2));
+    }
 
-	public static String[] readLetters() {
-		In in = new In(new File("src/test/resource/letters.txt"));
-		return in.readAllStrings();
-	}
+    @Test
+    public void equalItems() {
+        assertFalse(Sort.less(1, 1));
+    }
+
+    @Test
+    public void exchange() {
+        Integer[] i = new Integer[]{1, 2, 3, 4, 5, 6};
+        Sort.exchange(i, 4, 5);
+        assertTrue(i[4] == 6);
+        assertTrue(i[5] == 5);
+    }
+
+    public static <T extends Comparable<T>> boolean isSorted(T[] a) {
+        for (int i = 1; i < a.length; i++)
+            if (Sort.less(a[i], a[i - 1])) return false;
+        return true;
+    }
+
+    private static String[] readLetters() {
+        In in = new In(new File("src/test/resource/letters.txt"));
+        return in.readAllStrings();
+    }
+
+    @Test
+    public void sort() {
+        String[] s = readLetters();
+        sort(s, InsertionSort::sort);
+        sort(s, SelectionSort::sort);
+        sort(s, QuickSort::sort);
+        sort(s, MergeSort::sort);
+        sort(s, ShellSort::sort);
+    }
+
+    private static <T extends Comparable<T>> void sort(T[] t, Consumer<T[]> c) {
+        c.accept(t);
+        assertTrue(isSorted(t));
+    }
 
 }
